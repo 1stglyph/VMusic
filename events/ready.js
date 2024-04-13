d76e28907d9103d1f4de6c2671e0761f047a667e
1stglyph <166149435+1stglyph@users.noreply.github.com>
2024-04-13 14:05:47 +0300

    ready.js

--- events/ready.js
+++ events/ready.js
@@ -22,7 +22,7 @@ console.log('\x1b[36m%s\x1b[0m', '|    🚀 Commands Distracted!');
 console.log('\x1b[32m%s\x1b[0m', `|    🌼 Logged in as ${client.user.username}`);
 
 setInterval(() => client.user.setActivity({ 
-  name: `♫ | vlicvn`, 
+  name: `Rakı masasında ♫  `, 
   type: ActivityType.Listening }), 10000);
 client.errorLog = config.errorLog
 } else {
